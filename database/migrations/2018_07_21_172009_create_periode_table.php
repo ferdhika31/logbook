923a8fb284edc5f5bd07f157d623f95a6bf2eb0d
Ferdhika Yudira <rpl4rt08@gmail.com>
2018-07-22 00:24:17 +0700

    (+) Migration periode

---
+++ database/migrations/2018_07_21_172009_create_periode_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeriodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periode', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('no');
+            $table->date('tanggal_awal_periode');
+            $table->date('tanggal_akhir_periode');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('periode');
+    }
+}
